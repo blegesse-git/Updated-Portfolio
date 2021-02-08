1c9513b105397df78bb3f98a331802fad8a785d7
Beth Legesse <legessebl@gmail.com>
2021-02-08 15:02:16 -0600

    footer done

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import esthete from '../esthete.jpg';
-import litwine from '../Lit&Wine.JPG';
+import esthete from '../esthete.png';
+import litwine from '../Lit&Wine.png';
 import partie from '../partie.png'
 import fitnessApp from '../fitnessApp.jpg';
-import dayPlanner from '../dayPlanner.jpeg';
+import dayPlanner from '../dayPlanner.png';
 import employee from '../employeeSummary.png';
 import pwa from '../PWA.jpg';
 import noteTaker from '../noteTaker.jpeg';
@@ -48,11 +48,14 @@ function Portfolio() {
             <>
                 <img src={litwine} alt="lit and wine project" className="portfolio-image-popupbox" />
                 <p>
-                    Lit and Wine is a recommendation engine that helps an avid reader pair their choice of genre or author with the corresponding wine. For example: If you enjoy an intense book like a thriller/mystery it suggests a dry wine, or, on the other hand, if you enjoy a light read like a love story or a fantasy, it suggests a sweeter wine.
-
-                    Our vision for the app is to build a community for readers who can form a virtual book-club and create a profile. It will also enable them to review/critique books and wines they've tried as well as add their suggestions. Our motive is to provide a platform for people to share their profile with friends and family while exploring more books and wines.
-            </p>
+                    As a book reader I would like a one-stop shop application to find a prominent book I should read and add to my collection, and I would also like a recommendation of the best type of wine that would go with the book. 
+                    Lit and wine is a app readers can go to when they are feeling adventurous and they want to read books recommended by expert readers like my team and I. Our site features prominent authors whose work we've truly enjoyed. 
+                    These are hand picked authors and books who we believe everyone should read at least once in their life time... 
+                </p>
+                <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://blegesse-git.github.io/Lit-Wine/", "_blank")}>https://blegesse-git.github.io/Lit-Wine/</a>
+                <br />
                 <b>Github:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/blegesse-git/Lit-Wine")}>https://github.com/blegesse-git/Lit-Wine</a>
+
             </>
         )
         PopupboxManager.open({ content })
@@ -73,8 +76,8 @@ function Portfolio() {
             <>
                 <img src={partie} alt="partie project" className="portfolio-image-popupbox" />
                 <p>
-                    A party planning REACTJS app made for event planners where they can keep track of their upcoming planned events, track the budget, todos, invoices and RSVPs if needed, all on a clean, clutter free dashboard.
-            </p>
+                    Partie is a planning REACT app made for event planners to help them keep track of their upcoming events, track budgets, todo items, prepare invoices and send out RSVPs if needed, all on a clean and clutter free dashboard.
+                </p>
                 <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://powerful-fortress-17827.herokuapp.com/", "_blank")}>https://powerful-fortress-17827.herokuapp.com/</a>
                 <br />
                 <b>Github:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/sahibamj/Project-3")}>https://github.com/sahibamj/Project-3</a>
@@ -100,6 +103,8 @@ function Portfolio() {
                 <p>
                     This project is a simple daily planner that allows user to visually analyze the date and time based on color. Current time of the day shows red, future is highlighted in green, and past is in red. With a click of 'Save' button, user can store their input and have access to it even after closing the browser. The current date is always present and updated. Technologies Used: Moment.js, jQuery
             </p>
+                <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://blegesse-git.github.io/Day-Planner/", "_blank")}>https://blegesse-git.github.io/Day-Planner/</a>
+                <br />
                 <b>Github:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/blegesse-git/Day-Planner")}>https://github.com/blegesse-git/Day-Planner</a>
             </>
         )
@@ -121,8 +126,10 @@ function Portfolio() {
             <>
                 <img src={fitnessApp} alt="fitness tracker project" className="portfolio-image-popupbox" />
                 <p>
-                    As a user, I want to be able to view create and track daily workouts. I want to be able to log multiple exercises in a workout on a given day. I should also be able to track the name, type, weight, sets, reps, and duration of exercise. If the exercise is a cardio exercise, I should be able to track my distance traveled.
-            </p>
+                    Fitness App is a user friendly application that lets users view, create, and track daily workouts. User is able to log multiple exercises in a workout on a given day. The app also provides means to be able to track the name, type, weight, sets, reps, and duration of exercise. In case of cardio exercise, user can easily track distance traveled.
+                </p>
+                <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://secret-headland-22783.herokuapp.com/", "_blank")}>https://secret-headland-22783.herokuapp.com/</a>
+                <br />
                 <b>Github:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/blegesse-git/Fitness-Tracker")}>https://github.com/blegesse-git/Fitness-Tracker</a>
             </>
         )
@@ -144,14 +151,15 @@ function Portfolio() {
             <>
                 <img src={noteTaker} alt="Note taker project" className="portfolio-image-popupbox" />
                 <p>
-                    AS A user, I want to be able to write and save notes
-
-                    I WANT to be able to delete notes I've written before
-
-                    SO THAT I can organize my thoughts and keep track of tasks I need to complete.
-
+                    Note Taker is an app with cool features that lets user write and save notes.
+                    Want to scratch your notes and start over clean? That is no problem at all. 
+                    Note Taker also allows user to delete previously saved notes helping you 
+                    organize your thoughts and keep track of tasks you need to complete. 
+                   
                     Note taker application is an application that can be used to write, save, and delete notes. This application uses an express backend and save and retrieve note data from a JSON file.
-            </p>
+                </p>
+                <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://polar-sierra-14115.herokuapp.com/", "_blank")}>https://polar-sierra-14115.herokuapp.com/</a>
+                <br />
                 <b>Github:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/blegesse-git/Note-Taker")}>https://github.com/blegesse-git/Note-Taker</a>
             </>
         )
@@ -173,7 +181,10 @@ function Portfolio() {
             <>
                 <img src={employee} alt="employee summary engine project" className="portfolio-image-popupbox" />
                 <p>
-                    This is a software engineering team generator command line application. The application prompts the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns. When the user has completed building the team, the application will creare an HTML file that displays a nicely formatted team roster based on the information provided by the user.            </p>
+                    This is a software engineering team generator command line application. The application prompts the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns. When the user has completed building the team, the application will creare an HTML file that displays a nicely formatted team roster based on the information provided by the user.            
+                </p>
+                <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://drive.google.com/file/d/1WmiMj4KL-1Yu12dmPRVachqE7jm_VXSl/view", "_blank")}>https://drive.google.com/file/d/1WmiMj4KL-1Yu12dmPRVachqE7jm_VXSl/view</a>
+                <br />
                 <b>Github:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/blegesse-git/Template-Engine-Employee-summary")}>https://github.com/blegesse-git/Template-Engine-Employee-summary</a>
             </>
         )
@@ -195,20 +206,14 @@ function Portfolio() {
             <>
                 <img src={pwa} alt="pwa project" className="portfolio-image-popupbox" />
                 <p>
-                    Giving users a fast and easy way to track their money is important, but allowing them to access that information anytime is even more important. Having offline functionality is paramount to our applications success.
+                    Giving users a fast and easy way to track their money is important, but allowing them to access that information anytime is even more important. Having offline functionality is paramount to an application's success.
 
-                    The user will be able to add expenses and deposits to their budget with or without a connection. When entering transactions offline, they should populate the total when brought back online.
+                    With this app, users are able to add expenses and deposits to their budget with or without a connection. When entering transactions offline, the app automatically populate the total when brought back online.
 
-                    Offline Functionality:
-
-                    *Enter deposits offline
-
-                    *Enter expenses offline
-
-                    When brought back online:
-
-                    *Offline entries should be added to tracker.
+                    Offline Functionality includes: enter deposits offline, enter expenses offline. When brought back online, offline entries are added to tracker.
                 </p>
+                <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://powerful-escarpment-08226.herokuapp.com/", "_blank")}>https://powerful-escarpment-08226.herokuapp.com/</a>
+                <br />
                 <b>Github:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/blegesse-git/PWA-Budget_Tracker")}>https://github.com/blegesse-git/PWA-Budget_Tracker</a>
             </>
         )
@@ -232,6 +237,8 @@ function Portfolio() {
                 <p>
                 This is a React-based Google Books Search app that uses React lifecycle methods to query and display books based on user searches. The app also utilizes Node, Express and MongoDB so that users can save books to review or purchase later.
                 </p>
+                <b>Demo:</b> <a className="hyper-link" onClick={() => window.open("https://tranquil-badlands-34308.herokuapp.com/", "_blank")}>https://tranquil-badlands-34308.herokuapp.com//</a>
+                <br />
                 <b>Github:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/blegesse-git/Google-Book-Search")}>https://github.com/blegesse-git/Google-Book-Search</a>
             </>
         )
@@ -248,7 +255,7 @@ function Portfolio() {
     }
 
     return (
-        <div className="portfolio-wrapper">
+        <div id="portfolio" className="portfolio-wrapper">
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">Portfolio</h1>
                 <div className="image-box-wrapper row justify-content-center">
@@ -285,14 +292,8 @@ function Portfolio() {
                             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
                         </div>
                     </div>
-                    <div className="portfolio-image-box" onClick={openPopupboxNoteTaker}>
-                        <img className="portfolio-image" src={noteTaker} alt="note taker project" />
-                        <div className="overflow">
-                            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
-                        </div>
-                    </div>
-                    <div className="portfolio-image-box" onClick={openPopupboxEmployeeSummary}>
-                        <img className="portfolio-image" src={employee} alt="employee project" />
+                    <div className="portfolio-image-box" onClick={openPopupboxBookSearch}>
+                        <img className="portfolio-image" src={bookSearch} alt="Book Search project" />
                         <div className="overflow">
                             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
                         </div>
@@ -303,8 +304,17 @@ function Portfolio() {
                             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
                         </div>
                     </div>
-                    <div className="portfolio-image-box" onClick={openPopupboxBookSearch}>
-                        <img className="portfolio-image" src={bookSearch} alt="Book Search project" />
+                    <div className="portfolio-image-box" onClick={openPopupboxNoteTaker}>
+                        <img className="portfolio-image" src={noteTaker} alt="note taker project" />
+                        <div className="overflow">
+                            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+                        </div>
+                    </div>
+                    
+                    
+                    
+                    <div className="portfolio-image-box" onClick={openPopupboxEmployeeSummary}>
+                        <img className="portfolio-image" src={employee} alt="employee project" />
                         <div className="overflow">
                             <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
                         </div>
